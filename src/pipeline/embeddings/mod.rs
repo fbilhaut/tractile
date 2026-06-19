8671f976ed814e565e6382119023695df328a89e
Frédérik Bilhaut <frederik.bilhaut@sgevt.com>
2026-06-19 09:44:24 +0200

    refactor

--- src/pipeline/embeddings/mod.rs
+++ src/pipeline/embeddings/mod.rs
@@ -1,14 +1,11 @@
-mod mean_pool;
-mod raw;
-mod token;
+pub mod mean_pool;
+pub mod raw;
+pub mod token;
 
-use mean_pool::MeanPoolExtractor;
 use ndarray::Array2;
-use raw::RawExtractor;
-use token::TokenExtractor;
 use tract_onnx::prelude::*;
-
 use crate::config::ExtractorMode;
+
 
 pub trait Extractor {
     fn extract(
@@ -21,6 +18,7 @@ pub trait Extractor {
     ) -> crate::Result<Array2<f32>>;
 }
 
+
 impl Extractor for ExtractorMode {
     fn extract(
         &self,
@@ -32,13 +30,13 @@ impl Extractor for ExtractorMode {
     ) -> crate::Result<Array2<f32>> {
         match self {
             ExtractorMode::Raw => {
-                RawExtractor.extract(outputs, output_index, flat_mask, n, seq_len)
+                raw::RawExtractor.extract(outputs, output_index, flat_mask, n, seq_len)
             }
             ExtractorMode::Token(idx) => {
-                TokenExtractor(*idx).extract(outputs, output_index, flat_mask, n, seq_len)
+                token::TokenExtractor(*idx).extract(outputs, output_index, flat_mask, n, seq_len)
             }
             ExtractorMode::MeanPool => {
-                MeanPoolExtractor.extract(outputs, output_index, flat_mask, n, seq_len)
+                mean_pool::MeanPoolExtractor.extract(outputs, output_index, flat_mask, n, seq_len)
             }
         }
     }
